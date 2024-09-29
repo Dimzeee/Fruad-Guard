f006969aef57233080e46b04939a950e0289b34b
Commoit <edimanyong480@gmail.com>
2024-09-29 11:08:54 -0400

    Commit

--- main.py
+++ main.py
@@ -121,6 +121,7 @@ def fraud_trends(specific_user):
         response = openai.ChatCompletion.create(
             model="gpt-4o",
             messages=[{"role": "system", "content": prompt}]
+
         )
 
         advice = response['choices'][0]['message']['content']
